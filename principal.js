bb448e61d7b3506c6b5a85cd1d320016141f57b0
tainafer04 <163296424+tainafer04@users.noreply.github.com>
2024-09-23 08:35:30 -0300

    js titulo / imc

--- principal.js
+++ principal.js
@@ -1,2 +1,8 @@
 var titulo = document.querySelector("h1");
 titulo.textContent = "Aparecida Nutricionista";
+
+var dadosPaulo = document.querySelector('#linhaPaulo');
+var peso = dadosPaulo.querySelector('.info-peso').textContent;
+var altura = dadosPaulo.querySelector('.info-altura').textContent;
+var imc = peso / (altura * altura);
+dadosPaulo.querySelector('.info-imc').textContent = imc;
