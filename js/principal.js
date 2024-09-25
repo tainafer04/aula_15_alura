3fbf4f7597ac0653f85cc9638f94f97ad099d099
tainafer04 <163296424+tainafer04@users.noreply.github.com>
2024-09-25 08:20:19 -0300

    correção do código

--- js/principal.js
+++ js/principal.js
@@ -1,12 +1,6 @@
 var titulo = document.querySelector("h1");
 titulo.textContent = "Aparecida Nutricionista";
 
-var dadosPaulo = document.querySelector('#linhaPaulo');
-var peso = dadosPaulo.querySelector('.info-peso').textContent;
-var altura = dadosPaulo.querySelector('.info-altura').textContent;
-var imc = peso / (altura * altura);
-dadosPaulo.querySelector('.info-imc').textContent = imc;
-
 var pacientes = document.querySelectorAll('.paciente');
 for (let i = 0; i < pacientes.length; i++){
     var peso = pacientes[i].querySelector('.info-peso').textContent;
